e65e33ba3452c6ceef1a5627065a4d214b4fd857
guilherme0541 <guilherme0541@gmail.com>
2021-08-17 17:02:52 -0300

    Finish get started tutorial

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -8,7 +8,9 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+
+  @Output() notify = new EventEmitter();
 
   constructor() {}
 
